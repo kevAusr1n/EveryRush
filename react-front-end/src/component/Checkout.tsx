31009b7224ac9cde85130c6609d7fb2b3927b946
kevAusr1n <kevinausrain@gmail.com>
2025-02-07 23:24:17 +0800

    do checkout page

---
+++ react-front-end/src/component/Checkout.tsx
@@ -0,0 +1,18 @@
+function checkout() {
+    return (
+        <>
+            <div className="flex m-20">
+                <h1>Product Information</h1>
+            </div>
+            <div>
+                <h1>Post Address</h1>
+            </div>
+            <div>
+                <h1>Post Address</h1>
+            </div>
+            <div>
+                <h1>Billing Address</h1>
+            </div>
+        </>
+    )
+}
